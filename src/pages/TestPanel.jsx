caf174f7c319e502bf01ce7ec2b000057b63e717
Sanju Raj Prasad <43962657+Blazexsam27@users.noreply.github.com>
2023-05-24 09:01:52 +0530

    Added backend server

--- src/pages/TestPanel.jsx
+++ src/pages/TestPanel.jsx
@@ -1,120 +1,102 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import * as openai from "openai";
-import { Configuration, OpenAIApi } from "openai";
 import QuestionForm from "../component/QuestionForm";
 
-function TestPanel() {
+function TestPanel(props) {
   const Wrapper = styled.div`
     display: flex;
     width: 100vw;
     height: 100vh;
     justify-content: center;
+    flex-direction: column;
+    flex-wrap: wrap;
     align-items: center;
+    font-family: var(--josefin-font);
+    font-size: 1.3rem;
+    .instructions {
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      padding: 10px;
+    }
+    .start-btn {
+      border: none;
+      border-radius: 3px;
+      min-width: 100px;
+      padding: 10px 45px;
+      background: ${({ theme }) => theme.colors.sasquatch};
+      color: white;
+      transition: 0.4s;
+      &:hover {
+        transform: scale(1.05);
+        transition: 0.4s;
+      }
+    }
   `;
-
-  const configuration = new Configuration({
-    apiKey: import.meta.env.VITE_GPT_API,
-  });
-  delete configuration.baseOptions.headers["User-Agent"];
-  const openai = new OpenAIApi(configuration);
-
-  const [questions, setQuestions] = useState([]);
+  const subject = "javascript language";
+  const [questionSet, setQuestionSet] = useState([]);
+  const [selectedAnswers, setSelectedAnswers] = useState([]);
   const [index, setIndex] = useState(0);
-  const systemMessage = {
-    role: "system",
-    content: "Generate question for a intermediate level programmer.",
-  };
-
-  function processAPIResponse(mcq_questions) {
-    let question, options, lines;
-    mcq_questions.map((element) => {
-      lines = element.split("\n");
-      question = lines[1].replace("Question: ", "");
-      options = {
-        a: lines[3].slice(4),
-        b: lines[4].slice(4),
-        c: lines[5].slice(4),
-        d: lines[6].slice(4),
-      };
-      console.log("question", question);
-      console.log("options", options);
+  const getQuestions = async () => {
+    const response = await fetch("/api/test", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        prompt: `Please generate an MCQ with answer on ${subject} in this format\n Question: Which of the following options is correct?\n a) Option A\n b) Option B\n c) Option C\n d) Option D\n Answer: a)`,
+      }),
     });
-  }
-
-  function extractOptions(optionsString) {
-    const options = optionsString
-      .split("\n")
-      .map((option) => option.trim())
-      .filter((option) => option !== ""); // Filter out empty options
-
-    return options;
-  }
-
-  const generateQuestions = async () => {
-    const numQuestions = 5;
-
-    // Generate multiple questions
-    const prompts = Array(numQuestions).fill(
-      "### Multiple-Choice Question\n\nQuestion: Which of the following options is correct?\n\na) Option A\nb) Option B\nc) Option C\nd) Option D\n\nAnswer: "
-    );
-
-    const response = await openai.createCompletion({
-      engine: "text-davinci-003",
-      prompt: prompts,
-      maxTokens: 2048,
-      temperature: 0.5,
-    });
-    const parsedData = await response.json();
-    console.log(parsedData);
-    // .then((response) => {
-    //   const choices = response.choices;
-    //   const generatedQuestions = choices.map((choice) => {
-    //     const generatedQuestion = choice.text.trim();
-    //     const lines = generatedQuestion.split("\n");
-    //     const question = lines[0].replace("Question: ", "");
-    //     const options = {
-    //       a: lines[1].slice(4),
-    //       b: lines[2].slice(4),
-    //       c: lines[3].slice(4),
-    //       d: lines[4].slice(4),
-    //     };
-    //     const answer = lines[6].slice(8);
-    //     console.log("question", question);
-    //     console.log("options", options);
-    //     console.log("answer", answer);
-    //     return { question, options, answer };
-    //   });
-
-    //   setQuestions(generatedQuestions);
-    // })
-    // .catch((error) => {
-    //   console.error("Error:", error);
-    // });
-    // processAPIResponse(mcq_questions);
-  };
-
-  const evaluateResults = (e) => {
-    e.preventDefault();
-  };
-
-  const handleQuestionSubmit = (e) => {
-    e.preventDefault();
-    console.log(e.target);
+    const parsedResponse = await response.json();
+    console.log(parsedResponse.questions);
+    // setQuestionSet(parsedResponse.questions);
   };
 
   useEffect(() => {
-    generateQuestions();
+    // getQuestions();
   }, []);
+
+  /* testing json data
+   */
+  let tempQuestion = [
+    {
+      question: "what the dog doin?",
+      options: ["opt1", "opt2", "opt3", "opt4"],
+      answer: "a)",
+    },
+    {
+      question: "who was doing the party?",
+      options: ["opt1", "opt2", "opt3", "opt4"],
+      answer: "b)",
+    },
+  ];
 
   return (
     <Wrapper>
-      {/* <QuestionForm
-        handleQuestionSubmi={handleQuestionSubmit}
-        evaluateResults={evaluateResults}
-        question={questionList[index]}
-        options={optionsList[index]}
-      /> */}
+      <div className="instructions">
+        <b>Important Rules:</b>
+        <p>
+          ⭐ You'll be given a set of 10 questions based on your selected topics
+          with four options each.
+        </p>
+        <p>
+          ⭐ You'll be given a time limit of overall 10 mins to complete as many
+          questions as possible.
+        </p>
+        <p>
+          ⭐ Once a answer is submitted you cannot move to previous question.
+        </p>
+        <p>
+          ⭐ If timit limit expires all your test will be submitted
+          automatically and you will get the evaluated result.
+        </p>
+      </div>
+      <button className="start-btn">Start</button>
+      <QuestionForm
+        selectedAnswers={selectedAnswers}
+        setSelectedAnswers={setSelectedAnswers}
+        setIndex={setIndex}
+        index={index}
+        data={tempQuestion[index]}
+      />
     </Wrapper>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import replace from "@rollup/plugin-replace";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    replace({
-      "process.env.VITE_SHOWCASE_API": JSON.stringify(process.env.SHOWCASE_API),
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-    }),
-  ],
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
